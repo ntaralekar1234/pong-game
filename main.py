ffecc520e52c19ce1bc62db08ad748cff5b9f624
ntaralekar1234 <ntaralekar1234@gmail.com>
2025-01-01 20:27:42 +0530

    bounce ball when hit  lower or upper wall

--- main.py
+++ main.py
@@ -23,4 +23,10 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce()
+
+
 screen.exitonclick()
