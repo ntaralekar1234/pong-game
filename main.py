a7e9ad975c7f578690cd9d46fa2b37b8430c55df
ntaralekar1234 <ntaralekar1234@gmail.com>
2025-01-01 18:53:23 +0530

    paddle code refactor with inheritance

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from turtle import Screen
 from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=800)
+screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Paddle()
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
 
 screen.listen()
-screen.onkey(paddle.go_up(),"Up")
-screen.onkey(paddle.go_down(),"Down")
+screen.onkey(right_paddle.go_up,"Up")
+screen.onkey(right_paddle.go_down,"Down")
 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,16 +1,18 @@
 from turtle import Turtle
 
-class Paddle :
-    def __init__(self):
-        self.paddle = Turtle()
-        self.paddle.shape("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
