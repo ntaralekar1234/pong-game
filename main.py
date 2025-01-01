8d7499af59db954e057a1e3c5eaf2e368c213e76
ntaralekar1234 <ntaralekar1234@gmail.com>
2025-01-01 18:22:30 +0530

    paddle created

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from turtle import Screen
-
+from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=800)
 screen.title("Pong Game")
 
-
+paddle = Paddle()
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+class Paddle :
+    def __init__(self):
+        self.paddle = Turtle()
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+
+    def go_up(self):
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
