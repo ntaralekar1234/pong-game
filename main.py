280dd035287c26226bf649d3df56638687e0061d
ntaralekar1234 <ntaralekar1234@gmail.com>
2025-01-01 18:25:47 +0530

    paddle move functionality implemented

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=800)
 screen.title("Pong Game")
+screen.tracer(0)
 
 paddle = Paddle()
+
+screen.listen()
+screen.onkey(paddle.go_up(),"Up")
+screen.onkey(paddle.go_down(),"Down")
+
+
 screen.exitonclick()
